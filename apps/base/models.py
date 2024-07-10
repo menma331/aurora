15b1e79804eaefdc383e70dac6c51cbbe6cf8086
roman <romaxid@yandex.by>
2024-07-10 12:39:34 +0300

    feat: new model: BaseModel.

---
+++ apps/base/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+# class BaseManager(models.Manager):
+#     """Base manager for all models."""
+#
+#     def __delete__(self, instance):
+#         # TODO: Переписать метод delete(). Помечать как удаленный(deleted_at)
+#         pass
+#
+#     def __all__(self):
+#         pass
+
+
+class BaseModel(models.Model):
+    """Base model for all models.
+
+    Attributes:
+        objects (models.Manager): for getting access to orm.
+    """
+
+    objects = models.Manager()
+
+    # TODO: добавить общие поля.
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+    # deleted_at = models.DateTimeField(auto_now=True, default=None)
+    # f = models.ForeignKey(to='df', on_delete=models.PROTECT)
+
+    class Meta:
+        abstract = True
