0ef3cc513f9ed740b8fd20640a4f22c98783eb5e
roman <romaxid@yandex.by>
2024-07-10 12:38:14 +0300

    feat: new model: Tag.

---
+++ apps/user/models/tag.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from apps.base.models import BaseModel
+
+
+class Tag(BaseModel):
+    tag_name = models.CharField(
+        max_length=12, unique=True, db_index=True, verbose_name='Название тега'
+    )
+    user = models.ManyToManyField(
+        to='User',
+        related_name='tag',
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
